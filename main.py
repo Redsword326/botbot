846fd03554cf85cbfe65227307db131c3afcdb48
Noah <noahbedouch@gmail.com>
2019-12-29 00:49:38 -0800

    Fix my stupidity

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import discord
 import auth
 
-class MyClient(discord.Client) {
-    async def on_ready(self) {
-
-    }
-    async def on_message(self, message) {
-
-    }
-}
+class MyClient(discord.Client):
+    async def on_ready(self):
+        console.log(message)
+    async def on_message(self, message):
+        console.log(message)
 
 client = MyClient()
-client.run(auth.token);
+client.run(auth.token)
